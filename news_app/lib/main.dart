bb1278a9d84d6bf262a81b1e7b41af709b809766
Tarek Rashad <Tarek Rashad>
2024-08-09 08:27:56 +0300

    fix login page

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,13 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
+import 'package:news_app/Screens/Home_Page.dart';
 import 'package:news_app/Screens/Login_Page.dart';
+
 void main() async {
-   WidgetsFlutterBinding.ensureInitialized();
-     await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
-  
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+
+  //     // options: DefaultFirebaseOptions.currentPlatform,
+
+  //     );
   // await NewsServices(Dio()).getNews(category: 'general');
   runApp(const NewsWidget());
 }
@@ -21,7 +22,9 @@ class NewsWidget extends StatelessWidget {
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: const LoginScreen(),
-      // routes: {'speedNewsCardPage': (context) =>  SpeedNewsCardPage()},
+      routes: {
+        HomePage.id: (context) => const HomePage(),
+      },
     );
   }
 }
